b9e459170d4002b87047e2f293b09c3626822d89
RiteshKothawade-AC <ritesh.kothawade.cmaug26@gmail.com>
2026-08-18 23:06:09 +0530

    assign

--- Assignments/Logic Building/Assignment1/RectangleArea.java
+++
@@ -1,9 +0,0 @@
-public class RectangleArea {
-    public static void main(String[] args) {
-        int length = 10;
-        int width = 5;
-        int area = length * width;
-
-        System.out.println(area);
-    }
-}
--- Assignments/Logic Building/Assignment1/SquareArea.java
+++
@@ -1,8 +0,0 @@
-public class SquareArea {
-    public static void main(String[] args) {
-        int side = 5;
-        int area = side * side;
-
-        System.out.println(area);
-    }
-}
