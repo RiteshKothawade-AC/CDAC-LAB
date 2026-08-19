2553ab75c91db481e2be2cae7c95bc8168a1ce3a
RiteshKothawade-AC <ritesh.kothawade.cmaug26@gmail.com>
2026-08-19 21:02:05 +0530

    Assignments

---
+++ Assignments/Logic Building/Assignment1/EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd{
+    public static void main(String[] args) {
+        int i = 20;
+
+        if (i % 2 == 0) {
+            System.out.println(i + " is Even Number");
+        } else {
+            System.out.println(i + "is Odd Number");
+        }
+    }
+}
