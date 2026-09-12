45015e456f249098d878e93c668b081d28f7863f
RiteshKothawade-AC <ritesh.kothawade.cmaug26@gmail.com>
2026-09-12 23:12:15 +0530

    Add files via upload

---
+++ ASSIGNMENTS/OOPJAVA/EXERCISE/LOOPS/Pattern3.java
@@ -0,0 +1,19 @@
+public class Pattern3{
+	
+	public static void main(String[]args)
+	{
+		int num=1;
+		for(int itmp=1;itmp<=5;itmp++)
+		{
+			for(int jtmp=1;jtmp<itmp;jtmp++)
+			{
+				System.out.print(num +" ");
+				num++;
+			}
+			System.out.println( );
+		}
+		}
+		
+	
+
+}
---
+++ ASSIGNMENTS/OOPJAVA/EXERCISE/LOOPS/SeventhNumber.java
@@ -0,0 +1,13 @@
+
+public class SeventhNumber {
+	public static void main(String[]args)
+	{
+		System.out.println("Display Every Seventh Number from 1 to 200");
+		for(int itmp=1;itmp<=200;itmp++)
+		{
+			if(itmp%7==0)
+				System.out.println(itmp);
+		}
+	}
+
+}
